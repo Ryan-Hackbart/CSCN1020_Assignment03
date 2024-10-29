4879ebffafa481024e93b76c1c336cc609e32c1c
Ryan Hackbart <Rhackbart6884@conestogac.on.ca>
2024-10-29 12:00:55 -0400

    Add project files.

---
+++ Unit Test_RockPaperScissors/Unit Test_RockPaperScissors.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestRockPaperScissors
+{
+	TEST_CLASS(UnitTestRockPaperScissors)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
